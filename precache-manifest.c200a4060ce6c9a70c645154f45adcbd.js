7361ec155bb92a24da808858a81cdd6b82688450
Anastasija3587 <Anastasija3587gmail.com>
2020-01-10 12:06:10 +0200

    Updates

---
+++ precache-manifest.c200a4060ce6c9a70c645154f45adcbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ebe625e495c72f4cfd75b1c427e121",
+    "url": "/goit-react-hw03-image-finder/index.html"
+  },
+  {
+    "revision": "35f4ec523bb4eb216f5b",
+    "url": "/goit-react-hw03-image-finder/static/css/main.04544254.chunk.css"
+  },
+  {
+    "revision": "39f7245df7d7287929ac",
+    "url": "/goit-react-hw03-image-finder/static/js/2.aae20921.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/goit-react-hw03-image-finder/static/js/2.aae20921.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35f4ec523bb4eb216f5b",
+    "url": "/goit-react-hw03-image-finder/static/js/main.d99b931a.chunk.js"
+  },
+  {
+    "revision": "1cfd80ab38f01c5b96ed",
+    "url": "/goit-react-hw03-image-finder/static/js/runtime-main.0fc8bd29.js"
+  }
+]);
